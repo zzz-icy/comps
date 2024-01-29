5e17ee5721641851a7ef0611c8ef32fc8c2c7ab4
Hannah Z <eveline1129@163.com>
2024-01-29 13:43:21 -0600

    extracted dropdown page

---
+++ src/pages/DropdownPage.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef, useState } from "react"
+import { GoChevronDown, GoChevronLeft } from "react-icons/go"
+
+import "./Dropdown.css"
+import Panel from "../components/Panel"
+export interface Option {
+	label: string
+	value: string
+}
+
+interface Props {
+	options: Option[]
+	selection: Option | null
+	onSelect: (newSelection: Option) => void
+}
+
+function DropdownPage({ options, selection, onSelect }: Props) {
+	const [isOpen, setIsOpen] = useState<boolean>()
+	const divRef = useRef<HTMLDivElement>(null)
+
+	const handleClick = () => {
+		setIsOpen((currentIsOpen) => !currentIsOpen)
+	}
+
+	useEffect(() => {
+		const handler = (e) => {
+			if (!divRef.current) {
+				return
+			}
+			if (!divRef.current.contains(e.target)) {
+				setIsOpen(false)
+			}
+		}
+		document.addEventListener("click", handler, true)
+
+		return () => {
+			document.removeEventListener("click", handler)
+		}
+	}, [])
+
+	const handleOptionClick = (value: Option) => {
+		setIsOpen(false)
+		onSelect(value)
+	}
+
+	const renderedOptions = () => {
+		return options.map((option, index) => {
+			return (
+				<div
+					ref={divRef}
+					className='option'
+					key={option.value}
+					onClick={() => handleOptionClick(option)}
+				>
+					<div>{option.label}</div>
+				</div>
+			)
+		})
+	}
+	return (
+		<div className='container'>
+			<Panel
+				styleName='header'
+				onClick={handleClick}
+			>
+				{selection?.label || "Select..."}
+				{isOpen ? <GoChevronDown /> : <GoChevronLeft />}
+			</Panel>
+			{isOpen && <Panel styleName='sub-container'>{renderedOptions()}</Panel>}
+		</div>
+	)
+}
+export default DropdownPage
