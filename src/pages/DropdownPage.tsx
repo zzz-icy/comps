a6ef312c83525b7a67ba94f2a56fe668f7c39b31
Hannah Z <eveline1129@163.com>
2024-01-29 13:44:43 -0600

    more dropdown page

--- src/pages/DropdownPage.tsx
+++ src/pages/DropdownPage.tsx
@@ -1,72 +1,30 @@
-import React, { useEffect, useRef, useState } from "react"
-import { GoChevronDown, GoChevronLeft } from "react-icons/go"
+import Dropdown, { Option } from "../components/Dropdown.tsx"
+import "./index.css"
+import React, { useState } from "react"
+function DropdownPage() {
+	const options = [
+		{ label: "Red", value: "red" },
+		{ label: "Yellow", value: "yellow" },
+		{ label: "Green", value: "green" },
+	]
 
-import "./Dropdown.css"
-import Panel from "../components/Panel"
-export interface Option {
-	label: string
-	value: string
-}
-
-interface Props {
-	options: Option[]
-	selection: Option | null
-	onSelect: (newSelection: Option) => void
-}
-
-function DropdownPage({ options, selection, onSelect }: Props) {
-	const [isOpen, setIsOpen] = useState<boolean>()
-	const divRef = useRef<HTMLDivElement>(null)
-
-	const handleClick = () => {
-		setIsOpen((currentIsOpen) => !currentIsOpen)
+	const [selection, setSelection] = useState<Option | null>(null)
+	const handleSelection = (newSelection: Option) => {
+		setSelection(newSelection)
 	}
 
-	useEffect(() => {
-		const handler = (e) => {
-			if (!divRef.current) {
-				return
-			}
-			if (!divRef.current.contains(e.target)) {
-				setIsOpen(false)
-			}
-		}
-		document.addEventListener("click", handler, true)
-
-		return () => {
-			document.removeEventListener("click", handler)
-		}
-	}, [])
-
-	const handleOptionClick = (value: Option) => {
-		setIsOpen(false)
-		onSelect(value)
-	}
-
-	const renderedOptions = () => {
-		return options.map((option, index) => {
-			return (
-				<div
-					ref={divRef}
-					className='option'
-					key={option.value}
-					onClick={() => handleOptionClick(option)}
-				>
-					<div>{option.label}</div>
-				</div>
-			)
-		})
-	}
 	return (
-		<div className='container'>
-			<Panel
-				styleName='header'
-				onClick={handleClick}
-			>
-				{selection?.label || "Select..."}
-				{isOpen ? <GoChevronDown /> : <GoChevronLeft />}
-			</Panel>
-			{isOpen && <Panel styleName='sub-container'>{renderedOptions()}</Panel>}
+		<div style={{ display: "flex" }}>
+			<Dropdown
+				options={options}
+				selection={selection}
+				onSelect={handleSelection}
+			/>
+			<Dropdown
+				options={options}
+				selection={selection}
+				onSelect={handleSelection}
+			/>
 		</div>
 	)
 }
