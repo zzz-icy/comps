e64ec00439389bd3b2834bf2611e470a455c58d4
Hannah Z <eveline1129@163.com>
2024-01-29 14:24:41 -0600

    initially created navigation context

---
+++ src/context/navigation.tsx
@@ -0,0 +1,13 @@
+import React, { createContext } from "react"
+
+const NavigationContext = createContext({})
+
+const NavigationProvider = ({ children }) => {
+	return (
+		<NavigationContext.Provider value={{}}>
+			{children}
+		</NavigationContext.Provider>
+	)
+}
+export { NavigationProvider }
+export default NavigationContext
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
+import { NavigationProvider } from "./context/navigation.tsx"
 
 const el = document.getElementById("root")
 const root = ReactDOM.createRoot(el)
-root.render(<App />)
+root.render(
+	<NavigationProvider>
+		<App />
+	</NavigationProvider>
+)
