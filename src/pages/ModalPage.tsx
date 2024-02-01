77d7e274c044d700f1ca695273b2d6e8d9f4ecf1
Hannah Z <eveline1129@163.com>
2024-01-31 23:15:18 -0600

    mode modal

--- src/pages/ModalPage.tsx
+++ src/pages/ModalPage.tsx
@@ -1,9 +1,20 @@
 import React, { useState } from "react"
 import Modal from "../components/Modal.tsx"
+import Button from "../components/Button.tsx"
 function ModalPage() {
+	const [showModal, setShowModal] = useState<boolean>(false)
+	const handleClick = () => {
+		setShowModal(true)
+	}
 	return (
 		<div style={{ display: "flex" }}>
-			<Modal />
+			<Button
+				variation='primary'
+				// shapeStyle='rounded'
+				onClick={handleClick}
+			>
+				Open Modal
+			</Button>
 		</div>
 	)
 }
