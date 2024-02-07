67ee6596dbd104c3639d1b78890d2c04308a29fc
Hannah Z <eveline1129@163.com>
2024-02-06 21:57:34 -0600

    created config to dynamically render table data

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,35 +1,34 @@
 import React from "react"
 import "./Table.css"
+import { Fruit } from "../pages/TablePage"
 interface Props {
-	data: { name: string; color: string; score: number }[]
+	config: {
+		label: string
+		render: (data: Fruit) => any
+		// sort?: (a: number, b: number) => void
+	}[]
+	data: Fruit[]
 }
-function Table({ data }: Props) {
+
+function Table({ config, data }: Props) {
 	return (
 		<table>
 			<thead>
 				<tr>
-					<th>Fruit</th>
-					<th>Color</th>
-					<th>Score</th>
+					{config.map((column, index) => {
+						return <th key={column.label}>{column.label}</th>
+					})}
 				</tr>
 			</thead>
 
 			<tbody>
 				{/* single cell or single column */}
-				{data.map((item, index) => {
+				{data.map((fruit, index) => {
 					return (
-						<tr key={item.name}>
-							<td>{item.name}</td>
-							<td>
-								<div
-									style={{
-										backgroundColor: item.color,
-										margin: 5,
-										padding: 15,
-									}}
-								></div>
-							</td>
-							<td>{item.score}</td>
+						<tr key={fruit.name}>
+							{config.map((cell, index) => {
+								return <td>{cell.render(fruit)}</td>
+							})}
 						</tr>
 					)
 				})}
--- src/pages/TablePage.tsx
+++ src/pages/TablePage.tsx
@@ -1,17 +1,47 @@
 import Table from "../components/Table.tsx"
 import React from "react"
-
+const fruits = [
+	{ name: "Orange", color: "rgb(249 115 22)", score: 5 },
+	{ name: "Apple", color: "rgb(252 165 165)", score: 5 },
+	{ name: "Banana", color: "rgb(234 179 8)", score: 5 },
+	{ name: "Lime", color: "rgb(34 197 94)", score: 5 },
+]
+export interface Fruit {
+	name: string
+	color: string
+	score: number
+}
 function TablePage() {
-	const fruits = [
-		{ name: "Orange", color: "rgb(249 115 22)", score: 5 },
-		{ name: "Apple", color: "rgb(252 165 165)", score: 5 },
-		{ name: "Banana", color: "rgb(234 179 8)", score: 5 },
-		{ name: "Lime", color: "rgb(34 197 94)", score: 5 },
+	const config = [
+		{
+			label: "Name",
+			render: (fruit: Fruit) => fruit.name,
+		},
+		{
+			label: "Color",
+			render: (fruit: Fruit) => (
+				<div
+					style={{
+						backgroundColor: fruit.color,
+						margin: 5,
+						padding: 15,
+					}}
+				></div>
+			),
+		},
+		{
+			label: "Score",
+			render: (fruit: Fruit) => fruit.score,
+			// sort: (a, b) => {},
+		},
 	]
 
 	return (
 		<div>
-			<Table data={fruits} />
+			<Table
+				config={config}
+				data={fruits}
+			/>
 		</div>
 	)
 }
