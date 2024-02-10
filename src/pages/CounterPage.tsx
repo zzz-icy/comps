5fd7fbef94102db1aec73d57a92c52961106efea
Hannah Z <eveline1129@163.com>
2024-02-10 14:52:37 -0600

    started refactoring useState to useReducer

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,30 +1,46 @@
-import React, { BaseSyntheticEvent, SyntheticEvent, useState } from "react"
+import React, { BaseSyntheticEvent, useReducer, useState } from "react"
 import Button from "../components/Button.tsx"
 import Panel from "../components/Panel.tsx"
 import "./CounterPage.css"
-function CounterPage({ initialCount = 0 }) {
-	const [count, setCount] = useState<number>(initialCount)
-	const [valueToAdd, setValueToAdd] = useState<number>(0)
 
+const reducer = (state, action) => {
+	if (action.type === "increment") {
+		return {
+			...state,
+			count: state.count + 1,
+		}
+	}
+}
+
+function CounterPage({ initialCount = 0 }) {
+	// const [count, setCount] = useState<number>(initialCount)
+	// const [valueToAdd, setValueToAdd] = useState<number>(0)
+	const [state, dispatch] = useReducer(reducer, {
+		count: initialCount,
+		valueToAdd: 0,
+	})
 	const handleIncrement = () => {
-		setCount(count + 1)
+		// setCount(count + 1)
+		dispatch({
+			type: "increment",
+		})
 	}
 	const handleDecrement = () => {
-		setCount(count - 1)
+		// setCount(count - 1)
 	}
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		setCount(count + valueToAdd)
-		setValueToAdd(0)
+		// setCount(count + valueToAdd)
+		// setValueToAdd(0)
 	}
 	const handleChange = (e: BaseSyntheticEvent) => {
 		// will return NaN if an empty string is passed to parseInt
-		setValueToAdd(parseInt(e.target.value, 10) || 0)
+		// setValueToAdd(parseInt(e.target.value, 10) || 0)
 	}
 
 	return (
 		<Panel styleName='margin'>
-			<h1>Count is {count}</h1>
+			<h1>Count is {state.count}</h1>
 			<div style={{ display: "flex", flexDirection: "row" }}>
 				<Button
 					onClick={handleIncrement}
@@ -42,7 +58,7 @@ function CounterPage({ initialCount = 0 }) {
 			<form onSubmit={handleSubmit}>
 				<label>Add a lot!</label>
 				<input
-					value={valueToAdd || ""} // so that the 0 is always displayed
+					value={state.valueToAdd || ""} // so that the 0 is always displayed
 					onChange={handleChange}
 					type='number'
 					className='number-input'
